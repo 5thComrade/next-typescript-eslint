2e89474db9b010d505d249ee74bbe6f2fd00d984
Antony Chiramel <antonychiramel07@gmail.com>
2022-07-24 07:12:48 +0000

    Keep calm and commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = (props) => {
-  return <div>Hello World</div>;
+  return <div>Hello World from India</div>;
 };
 
 export default Home;
