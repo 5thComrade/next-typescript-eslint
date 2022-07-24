4501fed3c93f172afd1f0343b8cf2ba2af68226c
Antony Chiramel <antonychiramel07@gmail.com>
2022-07-24 14:53:46 +0000

    iport restrictions added

---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+function Component() {
+  return <div>Some Component</div>;
+}
+
+export default Component;
---
+++ src/containers/index.tsx
@@ -0,0 +1,5 @@
+function Container() {
+  return <div>Some Container</div>;
+}
+
+export default Container;
